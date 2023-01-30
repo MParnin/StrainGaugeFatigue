6270a541c196aa452cd2e8681d9828aa703dfa6b
Test Name <name@whatever.com>
2023-01-29 22:16:21 -0500

    Added SWT equation

--- readData.py
+++ readData.py
@@ -1,22 +1,36 @@
 import csv
+import math
 
 # Specify the file being read
 filename = "dataexample.txt"
 
-# Open the file in read mode
-with open(filename, "r") as file:
-    contents = file.read()
+def stress_amp(data): 
+    # Open the file in read mode
+    with open(data, "r") as file:
+        contents = file.read()
 
-# Split the contents by newline
-lines = contents.split("\n")
+        # Split the contents by newline
+        lines = contents.split("\n")
 
-# Create an emtpy list to store the lists of column values
-columns_list = []
+        # Create an emtpy list to store the lists of column values
+        columns_list = []
 
-# Iterate through the lines
-for line in lines:
-    values = line.split(",")
-    columns_list.append(values)
+        # Iterate through the lines
+        for line in lines:
+            values = line.split(",")
+            columns_list.append(values)
 
-# Print the lists to test
-print(columns_list[1])
+        # Testing: print the lists to test
+        # print(columns_list[1])
+
+        return columns_list
+    # Stress amplitude
+    S_a = 1
+
+    # Mean stress
+    S_m = 2
+
+    # Equivalent fully reversed stress amplitude using Smith-Watson-Topper's 
+    # model for mean stress effect
+
+    S_ar = sqrt((S_a + S_m)S_a)
